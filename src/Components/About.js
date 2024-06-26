7c0bb3e0aa8e2526ab8b84c679cb05ef8f7facf4
devil <saikartheek352@gmal.com>
2024-06-26 15:35:05 +0530

    new commit

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,32 +1,61 @@
 import React from 'react';
-import { LineChart } from '@mui/x-charts/LineChart';
+
+import Row from 'react-bootstrap/esm/Row';
+import Col from 'react-bootstrap/esm/Col';
+import SearchIcon from '@mui/icons-material/Search';
+import NotificationImportantIcon from '@mui/icons-material/NotificationImportant';
+import PeopleAltRoundedIcon from '@mui/icons-material/PeopleAltRounded';
+import { BarChart } from '@mui/x-charts/BarChart';
+
+
+
+const pData = [2400, 1398, -9800, 3908, 4800, -3800, 4300];
+const uData = [4000, -3000, -2000, 2780, -1890, 2390, 3490];
+
+const xLabels = [
+  'Page A',
+  'Page B',
+  'Page C',
+  'Page D',
+  'Page E',
+  'Page F',
+  'Page G',
+];
+
+
+
+
 function About() {
   return (
     <div>
-        <h1>My Dashboard</h1>
-        <div className='hiii'>
-          <h1 style={{fontSize:'20px'}}>Total Points</h1>
-          <div className='total'>
-          <p style={{float:'right'}}><i>from August 2018 to my 2019</i></p>
-          </div>
-        <LineChart
-      xAxis={[{ data: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] }]}
+        <Row>
+          <Col sm={6}>
+          <p>About Dashboard</p>
+          </Col>
+          <Col sm={2}>
+          <button  className='btn btn-primary'>Buy&Sell</button>
+          </Col>
+          <Col sm={2}>
+          <div id='bell'>
+          <SearchIcon/>
+         <NotificationImportantIcon/>
+         </div>
+          </Col>
+          <Col sm={2}>
+          <PeopleAltRoundedIcon style={{marginLeft:'70%',color:'white',fontSize:'30px'}}/>
+          </Col>
+        </Row>
+        <div className='pie'>
+        <BarChart
+      width={900}
+      height={500}
       series={[
-        {
-          data: [2, 3, 5.5, 8.5, 1.5, 5, 1, 4, 3, 8],
-          showMark: ({ index }) => index % 2 === 0,
-        },
+        { data: pData, label: 'pv', id: 'pvId', stack: 'stack1' },
+        { data: uData, label: 'uv', id: 'uvId', stack: 'stack1' },
       ]}
-      width={800}
-      height={300}
+      xAxis={[{ data: xLabels, scaleType: 'band' }]}
     />
-    <p1 >Aug 2022</p1>
-    <p1>Aug 2022</p1>
-    <p1>Aug 2022</p1>
-    <p1>Aug 2022</p1>
-    <p1>Aug 2022</p1>
-    <p1>Aug 2022</p1>
-     </div>
+    </div>
     </div>
   );
 }
